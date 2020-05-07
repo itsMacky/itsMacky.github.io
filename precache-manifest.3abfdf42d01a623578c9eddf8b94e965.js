e8a1aee7c0f2329f1693de739ca6567087ba74df
Macky <makrand586@gmail.com>
2020-05-07 15:03:12 +0530

    Updates

---
+++ precache-manifest.3abfdf42d01a623578c9eddf8b94e965.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0b26f26a0e6c3957a060c235936efb",
+    "url": "/itsMacky.github.io/index.html"
+  },
+  {
+    "revision": "8ae548cbab3189f97b16",
+    "url": "/itsMacky.github.io/static/css/main.c7719369.chunk.css"
+  },
+  {
+    "revision": "66b72f40688662330d86",
+    "url": "/itsMacky.github.io/static/js/2.fb6af289.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/itsMacky.github.io/static/js/2.fb6af289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ae548cbab3189f97b16",
+    "url": "/itsMacky.github.io/static/js/main.84a5a056.chunk.js"
+  },
+  {
+    "revision": "f7c0c5e7e359bc1611e9",
+    "url": "/itsMacky.github.io/static/js/runtime-main.5b571d72.js"
+  },
+  {
+    "revision": "fa1ecb92ef7b29348d069180b73ee13c",
+    "url": "/itsMacky.github.io/static/media/virus.fa1ecb92.webp"
+  }
+]);
